cf623c92dbf34bf63b3b6e4210058aae0e384b18
21343054 <112170832+21343054@users.noreply.github.com>
2022-11-21 07:07:15 +0700

    Add files via upload

---
+++ jobsheet11/src/LATIHAN1/Pertama.java
@@ -0,0 +1,13 @@
+
+public class Pertama {
+    private int a = 10;
+
+    protected void terprotek(){
+        System.out.println("Method ini hanya untuk anaknya");
+
+    }
+    public void info (){
+        System.out.println("a + "+ a);
+        System.out.println("dipanggil pada = "+this.getClass().getName());
+    }
+}
---
+++ jobsheet11/src/LATIHAN5/Bus.java
@@ -0,0 +1,11 @@
+package LATIHAN5;
+
+public class Bus {
+    public int penumpang;
+    public int maxPenumpang;
+
+    public void cetak(){
+        System.out.println("Penumpang Bus sekarang adalah "+ penumpang);
+        System.out.println("Penumpang maksimum seharusnya adalah "+ maxPenumpang);
+    }
+}
